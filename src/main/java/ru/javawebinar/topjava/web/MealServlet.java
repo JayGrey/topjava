513102a2ab297456786622e9bf798a8b410fda77
Jay Grey <jaygrey@yandex.ru>
2018-03-06 20:10:06 +0200

    ex 1.2.1

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,13 +1,21 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -17,6 +25,31 @@ public class MealServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         logger.debug("redirect ot meal");
+
+        List<Meal> meals = Arrays.asList(
+                new Meal(LocalDateTime.of(2018, Month.MARCH, 10, 10, 0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2018, Month.MARCH, 10, 13, 0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2018, Month.MARCH, 10, 20, 0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2018, Month.MARCH, 11, 10, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2018, Month.MARCH, 11, 13, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2018, Month.MARCH, 11, 20, 0), "Ужин", 510),
+                new Meal(LocalDateTime.of(2018, Month.MARCH, 12, 10, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2018, Month.MARCH, 12, 13, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2018, Month.MARCH, 12, 20, 0), "Ужин", 600)
+
+
+        );
+
+
+
+        List<MealWithExceed> mealWithExceeds = MealsUtil.getFilteredWithExceeded(meals, LocalTime.of(0, 0),
+                LocalTime.of(23, 59), 2000);
+        request.setAttribute("mealList", mealWithExceeds);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+        request.setAttribute("formatter", formatter);
+
+
         request.getRequestDispatcher("meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,10 +1,53 @@
-
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Meals</title>
+    <style>
+        .exceed {
+            color: red;
+        }
+
+        .normal {
+            color: green;
+        }
+    </style>
 </head>
 <body>
 
+<p>
+    <a href="<c:url value="/"/>">Home</a>
+
+</p>
+
+<table>
+    <thead>
+    <tr>
+        <td>Description</td>
+        <td>Calories</td>
+        <td>Date</td>
+    </tr>
+    </thead>
+    <%--@elvariable id="mealList" type="java.util.List<ru.javawebinar.topjava.model.MealWithExceed>"--%>
+    <%--<jsp:useBean id="formatter" class="java.time.format.DateTimeFormatter" />--%>
+    <c:forEach var="meal" items="${mealList}">
+        <c:choose>
+            <c:when test="${meal.exceed}">
+                <tr class="exceed">
+            </c:when>
+            <c:otherwise>
+                <tr class="normal">
+            </c:otherwise>
+        </c:choose>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+            <%--<td>${meal.dateTime.format(${formatter})}</td>--%>
+        <td>
+        ${meal.dateTime.format(<%%>)}
+        </td>
+        </tr>
+    </c:forEach>
+    
+</table>
 </body>
 </html>
