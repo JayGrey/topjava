0c44b7b4ef6f717d73c999ada269d473b2c07612
Jay Grey <jaygrey@yandex.ru>
2018-03-11 16:59:51 +0200

    task 5

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,19 +2,23 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.model.Role;
-import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.web.user.AdminRestController;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
 
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+//            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+//            Arrays.stream(appCtx.getBeanDefinitionNames()).forEach(System.out::println);
+//            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+//            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+            MealRestController mealRestController = appCtx.getBean(MealRestController.class);
+            Meal testMeal = mealRestController.create(new Meal(LocalDateTime.now(), "test meal", 100, AuthorizedUser.id()), AuthorizedUser.id());
+            System.out.println(mealRestController);
+            System.out.println(testMeal);
         }
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,50 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.time.LocalTime;
+import java.util.List;
+
+@Controller
 public class MealRestController {
+    @Autowired
     private MealService service;
+
+    public List<Meal> getAll(int userId) {
+        return service.getAll(userId);
+    }
+
+    public Meal get(int id, int userId) {
+        return service.get(id, userId);
+    }
+
+    public Meal create(Meal meal, int userId) {
+        return service.create(meal, userId);
+    }
+
+    public void delete(int id, int userId) {
+        service.delete(id, userId);
+    }
+
+    public void update(Meal meal, int userId) {
+        service.update(meal, userId);
+
+    }
+
+    public List<MealWithExceed> getAllWithExceed(int caloriesPerDay, int userId) {
+        List<Meal> meals = service.getAll(userId);
+        return MealsUtil.getWithExceeded(meals, caloriesPerDay);
+    }
+
+    public List<MealWithExceed> getFilteredWithExceed(int caloriesPerDay, LocalTime startTime, LocalTime endTime,
+                                                      int userId) {
+        List<Meal> meals = service.getAll(userId);
+        return MealsUtil.getFilteredWithExceeded(meals, caloriesPerDay, startTime, endTime);
+    }
 
 }
